068c3784dd2d27a30cc7f1d7e61bac0a963cc514
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-04-01 20:47:48 +0300

    no result message

--- src/bookList/BookList.js
+++ src/bookList/BookList.js
@@ -5,9 +5,11 @@ import './BookList.css';
 function BookList(props) {
     return (
         <div id="BookList">
-            {props.books.map((el, index) => {
+            {
+            props.books.length > 0 ? 
+            props.books.map((el, index) => {
                 return <Book {...el} key={el.id} index={index}/>
-            })}
+            }) : <div className="noResult">No result...</div>}
         </div>
     );
 }
